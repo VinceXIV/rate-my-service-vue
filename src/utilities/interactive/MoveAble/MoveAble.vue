b9cc7291d11d6c253cd264a9529dcfc99d7e7be4
Vincent <makokhavomondi@gmail.com>
2023-06-05 20:00:58 +0300

    Add utilities directory that will hold utility functions and data

---
+++ src/utilities/interactive/MoveAble/MoveAble.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+      <div class="target" ref="target">Vue Moveable</div>
+      <Moveable
+          className="moveable"
+          v-bind:target="['.target']"
+          v-bind:draggable="true"
+          v-bind:scalable="true"
+          v-bind:rotatable="true"
+          @drag="onDrag"
+          @scale="onScale"
+          @rotate="onRotate"
+      />
+  </div>
+</template>
+  <script>
+  import Moveable from "vue3-moveable";
+  
+  export default {
+    name: "MoveAble",
+    components: {
+      Moveable,
+    },
+    methods: {
+      onDrag({ transform }) {
+          this.$refs.target.style.transform = transform;
+      },
+      onScale({ drag }) {
+          this.$refs.target.style.transform = drag.transform;
+      },
+      onRotate({ drag }) {
+          this.$refs.target.style.transform = drag.transform;
+      },
+    }
+  }
+</script>
+
+<style>
+  .target {
+    width: 10rem;
+    margin: auto;
+    margin-top: 10rem;
+  }
+</style>
