f192212081983796e2a7936989bec4076db77d3c
Vincent <makokhavomondi@gmail.com>
2023-06-06 11:56:29 +0300

    Create canvas component that will hold items in the canvas

---
+++ src/components/Canvas/CanvasComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="canvas" v-for="object in objectArray" :key="object.id">
+        <Moveable  v-bind:object="object"/>
+    </div>
+</template>
+
+<script>
+    import Moveable from "../../utilities/interactive/MoveAble/MoveAble.vue"
+
+    export default {
+        name: 'CanvasObject',
+        props: {
+            objectArray: Array
+        },
+        components: {
+            Moveable
+        }
+    }   
+</script>
+
+<style>
+    
+</style>
